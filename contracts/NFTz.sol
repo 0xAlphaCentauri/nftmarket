6c84d5e708ca09aa229eb60ddfd20dfd3d2a3ae3
Zombie <mmuscarnera90@hotmail.com>
2024-05-10 11:14:44 -0700

    contract

---
+++ contracts/NFTz.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
+contract NFTz is ERC721URIStorage{
+    uint public tokenCount;
+
+    constructor() ERC721("nft Market", "NFTM"){
+
+    }
+
+    function mint(string memory _tokenURI) external returns (uint){
+        tokenCount ++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return(tokenCount);
+    }
+}
---
+++ src/frontend/components/App.js
@@ -0,0 +1,30 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+  } from "react-router-dom";
+  // import Navigation from './Navbar';
+  // import Home from './Home.js'
+  // import Create from './Create.js'
+  // import MyListedItems from './MyListedItems.js'
+  // import MyPurchases from './MyPurchases.js'
+  // import MarketplaceAbi from '../contractsData/Marketplace.json'
+  // import MarketplaceAddress from '../contractsData/Marketplace-address.json'
+  // import NFTAbi from '../contractsData/NFT.json'
+  // import NFTAddress from '../contractsData/NFT-address.json'
+  import { useState } from 'react'
+  import { ethers } from "ethers"
+  import { Spinner } from 'react-bootstrap'
+  
+  import './App.css';
+  
+  function App() {
+    
+  
+    return (
+      <></>
+  
+    );
+  }
+  
+  export default App;
